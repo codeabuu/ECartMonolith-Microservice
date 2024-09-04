c4aa070e4107498703eca40c0725958ce68eb79d
codeabuu <abdulngui718@gmail.com>
2024-09-04 18:24:26 -0400

    updates

---
+++ pkg/orders/domain/orders/product.go
@@ -0,0 +1,36 @@
+package orders
+
+import (
+	"errors"
+
+	"github.com/codeabuu/ECartMonolith-Microservice/pkg/common/price"
+)
+
+type ProductID string
+
+var ErrEmptyProduct = errors.New("Empty product ID")
+
+type Product struct {
+	id    ProductID
+	name  string
+	price price.Price
+}
+
+func (p Product) ID() ProductID {
+	return p.id
+}
+
+func (p Product) Name() string {
+	return p.name
+}
+
+func (p Product) Price() price.Price {
+	return p.price
+}
+
+func NewProduct(id ProductID, name string, price price.Price) (Product, error) {
+	if len(id) == 0 {
+		return Product{}, ErrEmptyProduct
+	}
+	return Product{id, name, price}, nil
+}
