40c07bc7029c6983cb8f463e18f7c5dde031a4aa
codeabuu <abdulngui718@gmail.com>
2024-09-11 18:26:51 -0400

    Implemented the main shop application, product domain logic, repository pattern, and in-memory storage

--- cmd/microservices/shop/main.go
+++ cmd/microservices/shop/main.go
@@ -5,25 +5,33 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/codeabuu/ECartMonolith-Microservice/pkg/common/cmd"
+	"github.com/codeabuu/ECartMonolith-Microservice/pkg/shop"
+	shop_app "github.com/codeabuu/ECartMonolith-Microservice/pkg/shop/application"
+	shop_infra_product "github.com/codeabuu/ECartMonolith-Microservice/pkg/shop/infrastructure/products"
+	shop_interfaces_private_http "github.com/codeabuu/ECartMonolith-Microservice/pkg/shop/interfaces/private/http"
+	shop_interfaces_public_http "github.com/codeabuu/ECartMonolith-Microservice/pkg/shop/interfaces/public/http"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	log.Println("starting shop ms...")
+	log.Println("Starting shop microservice..")
 
 	ctx := cmd.Context()
 
 	r := createShopMicroservice()
 
-	server := &http.Server{Addr: os.Getenv("SHOP_SHOP_PRODUCT_SERVICE_BIND_ADDR"), Handler: r}
+	server := &http.Server{Addr: os.Getenv("SHOP_SHOP_SERVICE_BIND_ADDR"), Handler: r}
 
 	go func() {
 		if err := server.ListenAndServe(); err != http.ErrServerClosed {
 			panic(err)
 		}
 	}()
+
 	<-ctx.Done()
 	log.Println("Closing shop microservice")
+
 	if err := server.Close(); err != nil {
 		panic(err)
 	}
@@ -31,6 +39,11 @@ func main() {
 
 func createShopMicroservice() *chi.Mux {
 	shopProductRepo := shop_infra_product.NewMemoryRepository()
+	shopProductsService := shop_app.NewProductsService(shopProductRepo, shopProductRepo)
+
+	if err := shop.LoadShopFixtures(shopProductsService); err != nil {
+		panic(err)
+	}
 
 	r := cmd.CreateRouter()
 
